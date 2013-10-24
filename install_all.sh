c1ff08458a36fbefdf26883c0d8650280c86fe53
boardstretcher <boardstretcher@gmail.com>
2013-10-24 12:36:12 -0400

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,12 +1,28 @@
 # tested on Scientific Linux 6.4 x64
- 
+
 # for a clean minimal installation and configuration of SL6,
 # see this gist: https://gist.github.com/boardstretcher/6655517
- 
+
+# this will install:
+ # ruby
+ # mysql
+ # puppet
+ # puppet dashboard
+ # puppetdb
+ # passenger
+ # activemq
+ # mcollective
+
+echo "the steps: set these variables"
+
 echo "hostname: ${HOSTNAME}"
 echo "domain: ${DOMAIN}"
-echo "if empty, you want to set it in network/hosts and reboot (FQDN), or set them manually"
-echo "with export"
+
+echo "then edit this script and change any instance of 'somepassword' to something else"
+
+echo "then remove exit; from this file and run it"
+
+exit;
 
 # most current version (12-Apr-2013)
 rpm -ivh https://yum.puppetlabs.com/el/6.4/products/x86_64/puppetlabs-release-6-7.noarch.rpm
@@ -14,7 +30,22 @@ rpm -ivh http://mirrors.mit.edu/epel/6/x86_64/epel-release-6-8.noarch.rpm
 
 # update system, install needed programs
 yum update -y
-yum install -y vim ntp wget openssh-clients
+yum install -y vim ntp wget openssh-clients openssl-devel zlib-devel \
+    gcc gcc-c++ make autoconf readline-devel curl-devel expat-devel \ 
+    gettext-devel 
+
+# get newest ruby/libyaml
+wget http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz
+tar xzvf yaml-0.1.4.tar.gz; cd yaml-0.1.4
+./configure --prefix=/usr/local
+make; make install
+
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p0.tar.gz
+tar xzvf ruby-1.9.3-p0.tar.gz; cd ruby-1.9.3-p0
+./configure --prefix=/usr/local --enable-shared --disable-install-doc --with-opt-dir=/usr/local/lib
+make; make install
+
+ruby -v; gem --version
 
 # fix time
 ntpdate pool.ntp.org
@@ -83,4 +114,16 @@ username: dashboard_dev
 password: somepassword
 encoding: utf8
 adapter: mysql
+EOF
+
+cd /usr/share/puppet-dashboard
+rake RAILS_ENV=development db:migrate
+rake RAILS_ENV=production db:migrate
+
+cat << EOF > /etc/puppet/puppet.conf
+[agent]
+report = true
+[master]
+reports = store, http
+reporturl = http://${HOSTNAME}:3000/reports/upload"
 EOF
