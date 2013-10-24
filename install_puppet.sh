3f73b55612d891e2519b9cea00aa7bd5598b273f
boardstretcher <boardstretcher@gmail.com>
2013-10-24 12:13:26 -0400

    Create install_puppet.sh

---
+++ install_puppet.sh
@@ -0,0 +1,36 @@
+# most importantly, make sure your hostname and domain is set and correct
+# /etc/sysconfig/network
+# /etc/hosts
+echo "hostname: ${HOSTNAME}"
+echo "if empty, you want to set it in network/hosts and reboot"
+ 
+# most current version (12-Apr-2013)
+rpm -ivh https://yum.puppetlabs.com/el/6.4/products/x86_64/puppetlabs-release-6-7.noarch.rpm
+rpm -ivh http://mirrors.mit.edu/epel/6/x86_64/epel-release-6-8.noarch.rpm
+ 
+# update system, install needed programs
+yum update -y
+yum install -y vim ntp
+ 
+# fix time
+ntpdate pool.ntp.org
+ 
+################# reboot!
+sleep 10
+ 
+# install puppet, client and dashboard
+yum install -y puppet-server puppet
+ 
+# start, create cert
+service puppetmaster start
+puppet cert --generate ${HOSTNAME}
+ 
+# config the puppetmaster
+echo "PUPPET_SERVER=${HOSTNAME}" > /etc/sysconfig/puppet
+echo "PUPPET_LOG=/var/log/puppet/puppet.log" >> /etc/sysconfig/puppet
+echo "PUPPET_EXTRA_OPTS=--waitforcert=500" >> /etc/sysconfig/puppet
+ 
+# start, clear cache, create client cert
+rm -f /var/lib/puppet/ssl/certs/localhost.localdomain.pem
+service puppet start
+puppet agent --waitforcert 60 --test --debug
