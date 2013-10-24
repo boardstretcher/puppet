2871bdb178f1ec4b1d63f3a7c61a0e291c4f4d39
boardstretcher <boardstretcher@gmail.com>
2013-10-24 16:51:21 -0400

    Update install_puppet.sh

--- install_puppet.sh
+++ install_puppet.sh
@@ -2,6 +2,7 @@
 # /etc/sysconfig/network
 # /etc/hosts
 echo "hostname: ${HOSTNAME}"
+echo "mysqlpassword: ${MYSQLPASSWORD}"
 echo "if empty, you want to set it in network/hosts and reboot"
  
 # most current version (12-Apr-2013)
@@ -19,13 +20,64 @@ ntpdate pool.ntp.org
 sleep 10
  
 # install puppet, client and dashboard
-yum install -y puppet-server
+yum install -y puppet-server puppetdb-terminus puppetdb
  
 # config the puppetmaster
 echo "PUPPET_SERVER=${HOSTNAME}" > /etc/sysconfig/puppet
 echo "PUPPET_LOG=/var/log/puppet/puppet.log" >> /etc/sysconfig/puppet
 echo "PUPPET_EXTRA_OPTS=--waitforcert=60" >> /etc/sysconfig/puppet
- 
+echo "*" > /etc/puppet/autosign.conf
+
+# puppet db config
+cat << EOF > /etc/puppet/puppetdb.conf
+[main]
+server = ${HOSTNAME}
+port = 8081
+EOF
+
+cat << EOF > /etc/puppet/routes.yaml 
+---
+master:
+facts:
+terminus: puppetdb
+cache: yaml
+EOF
+
+cat << EOF > /etc/puppet/puppet.conf
+[main]
+logdir = /var/log/puppet
+rundir = /var/run/puppet
+ssldir = /etc/puppet/ssl
+[agent]
+classfile = /etc/puppet/classes.txt
+localconfig = /etc/puppet/localconfig
+[master]
+storeconfigs = true
+storeconfigs_backend = puppetdb
+EOF
+
 # start, create cert
-service puppetmaster start
+service puppetmaster restart
 puppet cert --generate ${HOSTNAME}
+/usr/sbin/puppetdb-ssl-setup
+service puppetdb restart
+service puppetmaster restart
+chkconfig puppetmaster on
+chkconfig puppetdb on
+
+mkdir /etc/puppet/files
+chown -R puppet.puppet /etc/puppet
+
+# set up fileserver
+cat << EOF > /etc/puppet/fileserver.conf
+[files]
+path /etc/puppet/files
+allow *.${DOMAIN}
+EOF
+
+yum install -y puppet-dashboard mysql mysql-server
+chkconfig mysqld on 
+service mysqld start
+mysql -u root -e "CREATE DATABASE dashboard";
+mysql -u root -e "GRANT ALL PRIVILEGES ON dashboard.* TO dashboard@localhost IDENTIFIED BY '${MYSQLPASSWORD}';"
+
