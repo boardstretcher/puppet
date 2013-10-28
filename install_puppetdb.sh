7f1dc82befa9ecd36efbde0120476f7be3515374
boardstretcher <boardstretcher@gmail.com>
2013-10-28 16:09:54 -0400

    Update install_puppetdb.sh

--- install_puppetdb.sh
+++ install_puppetdb.sh
@@ -6,6 +6,11 @@
 # lets make sure that the hostname is all good
 facter | grep host
 
+echo "That hostname should be a FQDN, if it is not, then exit this installation";
+echo "and set your /etc/hosts, /etc/sysconfig/network files to have the FQDN and reboot";
+echo ""
+echo "Press enter to coninute or CTLR-C to quit and make changes"; read
+
 # install the module
 puppet module install puppetlabs/puppetdb
 
@@ -17,3 +22,8 @@ puppet resource service puppetdb ensure=running enable=true
 
 # check for errors
 tail /var/log/puppetdb/puppetdb.log
+
+# edit 
+# puppetdb.conf
+# puppet.conf
+# routes.yaml
