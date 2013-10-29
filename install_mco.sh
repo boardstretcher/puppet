52e9df037d3a167462b63a7a0fbb71acc9cb80d3
boardstretcher <boardstretcher@gmail.com>
2013-10-29 12:38:12 -0400

    Update install_mco.sh

--- install_mco.sh
+++ install_mco.sh
@@ -2,5 +2,10 @@
 
 puppet module install puppetlabs/mcollective
 puppet resource package mcollective ensure=latest --server ${FQDN}
+puppet resource package mcollective-client ensure=latest --server ${FQDN}
 puppet resource service mcollective ensure=running enable=true
+
+sed -i "s/localhost/${FQDN}/g" /etc/mcollective/server.cfg
+service mcollective restart
+
 
