f83fde71fd27515587a1998b29d15fe1a8c4419b
boardstretcher <boardstretcher@gmail.com>
2013-10-29 13:14:53 -0400

    Update install_passenger.sh

--- install_passenger.sh
+++ install_passenger.sh
@@ -4,3 +4,38 @@
 yum install -y httpd httpd-devel mod_ssl ruby-devel rubygems gcc make
 gem install rack passenger
 passenger-install-apache2-module
+
+mkdir -p /usr/share/puppet/rack/puppetmasterd
+mkdir /usr/share/puppet/rack/puppetmasterd/public /usr/share/puppet/rack/puppetmasterd/tmp
+cp /usr/share/puppet/ext/rack/files/config.ru /usr/share/puppet/rack/puppetmasterd/
+chown puppet /usr/share/puppet/rack/puppetmasterd/config.ru
+
+cat << EOF > /etc/httpd/conf.d/puppetmaster.conf
+LoadModule passenger_module /usr/lib/ruby/gems/1.8/gems/passenger-4.0.21/buildout/apache2/mod_passenger.so
+PassengerRoot /usr/lib/ruby/gems/1.8/gems/passenger-4.0.21
+PassengerDefaultRuby /usr/bin/ruby
+PassengerHighPerformance On
+PassengerUseGlobalQueue On
+PassengerMaxPoolSize 4
+PassengerMaxRequests 1000
+PassengerPoolIdleTime 600
+
+Listen 8140
+<VirtualHost *:8140>
+    RackAutoDetect On
+    DocumentRoot /usr/share/puppet/rack/puppetmasterd/public/
+    <Directory /usr/share/puppet/rack/puppetmasterd/>
+        Options None
+        AllowOverride None
+        Order Allow,Deny
+        Allow from All
+    </Directory>
+</VirtualHost>
+EOF
+
+echo "ServerName ${FQDN}" >> /etc/httpd/conf/httpd.conf
+
+service puppetmaster stop
+chkconfig puppetmaster off
+
+service httpd restart
