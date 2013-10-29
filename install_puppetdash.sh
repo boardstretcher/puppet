2f164c7110fb439ad75fa4fb8dbfc384cb7695d4
boardstretcher <boardstretcher@gmail.com>
2013-10-29 12:13:33 -0400

    Update install_puppetdash.sh

--- install_puppetdash.sh
+++ install_puppetdash.sh
@@ -10,41 +10,39 @@ mysql_secure_installation
 mysql -e "create database puppetdashprod"
 mysql -e "create database puppetdashdev"
 mysql -e "create database puppetdashtest"
-mysql -e "grant all on puppetdashprod.* to 'puppetdashprod'@'localhost' identified by 'puppetdashprod';"
-mysql -e "grant all on puppetdashdev.* to 'puppetdashdev'@'localhost' identified by 'puppetdashdev';"
-mysql -e "grant all on puppetdashtest.* to 'puppetdashtest'@'localhost' identified by 'puppetdashtest';"
-mysql -e "grant all on puppetdashprod.* to 'puppetdashprod'@'%' identified by 'puppetdashprod';"
-mysql -e "grant all on puppetdashdev.* to 'puppetdashdev'@'%' identified by 'puppetdashdev';"
-mysql -e "grant all on puppetdashtest.* to 'puppetdashtest'@'%' identified by 'puppetdashtest';"
+mysql -e "grant all on puppetdashprod.* to 'puppetdashprod'@'localhost' identified by '${MYSQLPASSWORD}';"
+mysql -e "grant all on puppetdashdev.* to 'puppetdashdev'@'localhost' identified by '${MYSQLPASSWORD}';"
+mysql -e "grant all on puppetdashtest.* to 'puppetdashtest'@'localhost' identified by '${MYSQLPASSWORD}';"
+mysql -e "grant all on puppetdashprod.* to 'puppetdashprod'@'%' identified by '${MYSQLPASSWORD}';"
+mysql -e "grant all on puppetdashdev.* to 'puppetdashdev'@'%' identified by '${MYSQLPASSWORD}';"
+mysql -e "grant all on puppetdashtest.* to 'puppetdashtest'@'%' identified by '${MYSQLPASSWORD}';"
  
 # puppetdash config
 cat << EOF > /usr/share/puppet-dashboard/config/database.yml
 production:
   database: puppetdashprod
   username: puppetdashprod
-  password: puppetdashprod
+  password: ${MYSQLPASSWORD}
   encoding: utf8
   adapter: mysql
 
 development:
   database: puppetdashdev
   username: puppetdashdev
-  password: puppetdashdev
+  password: ${MYSQLPASSWORD}
   encoding: utf8
   adapter: mysql
 
 test:
   database: dashboardtest
   username: dashboardtest
-  password: dashboardtest
+  password: ${MYSQLPASSWORD}
   encoding: utf8
   adapter: mysql
 EOF
+
 echo "THOSE TWO SPACES BEFORE EACH LINE MATTER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo "So be sure to make sure that each line (not production, development, or test) have two spaces"
-echo "before it... I will open up VIM for you to do so. Press enter to open VIM."; read
-vim /usr/share/puppet-dashboard/config/database.yml
- 
+
 # final setup
 cd /usr/share/puppet-dashboard
 rake gems:refresh_specs
