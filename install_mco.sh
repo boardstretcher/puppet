ff760ac57c1a9e262f266632530452efe35d3d36
boardstretcher <boardstretcher@gmail.com>
2013-10-29 12:47:21 -0400

    Update install_mco.sh

--- install_mco.sh
+++ install_mco.sh
@@ -6,6 +6,15 @@ puppet resource package mcollective-client ensure=latest --server ${FQDN}
 puppet resource service mcollective ensure=running enable=true
 
 sed -i "s/localhost/${FQDN}/g" /etc/mcollective/server.cfg
+sed -i "s/localhost/${FQDN}/g" /etc/mcollective/client.cfg
+
+service puppetmaster restart
+service puppetdb restart
+service activemq restart
 service mcollective restart
 
+service puppetmaster status
+service puppetdb status
+service activemq status
+service mcollective status
 
