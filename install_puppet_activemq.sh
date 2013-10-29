6761d0bce3b126bc4b1bce1608bccd28d5bdbcfd
boardstretcher <boardstretcher@gmail.com>
2013-10-29 11:25:03 -0400

    Update install_puppet_activemq.sh

--- install_puppet_activemq.sh
+++ install_puppet_activemq.sh
@@ -1,5 +1,7 @@
 # installation of activeMQ for mcollective
 
 puppet module install puppetlabs/activemq
+puppet resource package activemq ensure=latest --server ${FQDN}
+puppet resource service activemq ensure=running enable=true
 
 # that was easy!
