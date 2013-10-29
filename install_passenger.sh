52b0d7383778dc9937c637896afec5b58f1f5dec
boardstretcher <boardstretcher@gmail.com>
2013-10-29 14:00:08 -0400

    Update install_passenger.sh

--- install_passenger.sh
+++ install_passenger.sh
@@ -5,11 +5,13 @@ yum install -y httpd httpd-devel mod_ssl ruby-devel rubygems gcc make
 gem install rack passenger
 passenger-install-apache2-module
 
+# directories and files required
 mkdir -p /usr/share/puppet/rack/puppetmasterd
 mkdir /usr/share/puppet/rack/puppetmasterd/public /usr/share/puppet/rack/puppetmasterd/tmp
 cp /usr/share/puppet/ext/rack/files/config.ru /usr/share/puppet/rack/puppetmasterd/
 chown puppet /usr/share/puppet/rack/puppetmasterd/config.ru
 
+# the apache configuration
 cat << EOF > /etc/httpd/conf.d/puppetmaster.conf
 LoadModule passenger_module /usr/lib/ruby/gems/1.8/gems/passenger-4.0.21/buildout/apache2/mod_passenger.so
 PassengerRoot /usr/lib/ruby/gems/1.8/gems/passenger-4.0.21
@@ -35,7 +37,12 @@ EOF
 
 echo "ServerName ${FQDN}" >> /etc/httpd/conf/httpd.conf
 
+# stop webrick server
 service puppetmaster stop
 chkconfig puppetmaster off
 
+# start apache puppetmaster server
 service httpd restart
+
+# turn off autosigning
+sed -i 's/autosign\ \=\ true/autosign\ \=\ false/g' /etc/puppet/puppet.conf
