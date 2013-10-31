b1912b065423978a6d2b50d9ddcfb08f010c3d13
boardstretcher <boardstretcher@gmail.com>
2013-10-31 16:51:55 -0400

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,7 @@
 # ONLY TESTED ON Centos 6.4 x86_64
 
 
-
-# Warning - I disable SELINUX and IPTABLES
-
+# Warning - I disable SELINUX and IPTABLES and install GCC
 
 
 # prepare server for puppet installation
